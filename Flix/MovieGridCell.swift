88097e7048378e3d238a95d48fd2bccc770d7382
Yuchong Lee <leeyuchong@outlook.com>
2021-11-30 18:15:42 -0500

    Completed Flix Pt 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Yuchong Lee on 11/24/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
